2e614d65c817b7010ecbffa44fa46a7c0c6b29e6
jerikls <jhon.avila@loadsure.net>
2023-02-22 00:38:31 -0500

    feat: providing context

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Home from './views/home';
+import { ProductsProvider } from './providers/ProductsProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <ProductsProvider>
+      <Home />
+    </ProductsProvider>
   </React.StrictMode>
 );
 
---
+++ src/providers/ProductsProvider.js
@@ -0,0 +1,13 @@
+import { useState, createContext } from 'react'
+
+export const ProductsContext = createContext();
+
+export const ProductsProvider = props => {
+  const [products, setProducts] = useState();
+
+  return (
+    <ProductsContext.Provider value={{ products, setProducts }} >
+      { props.children }
+    </ProductsContext.Provider>
+  )
+}
