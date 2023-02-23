7fffd0e3a407755eb6c1d79441e58cdb4bf96971
jerikls <jhon.avila@loadsure.net>
2023-02-23 00:02:13 -0500

    feat: product card component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,25 +6,23 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-export default function ProductCard() {
+export default function ProductCard({ product }) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
+        image={product.image}
+        title={product.title}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+        { `${product.title.slice(0, 20)}...` }
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          { `${product.description.slice(0, 100)}...` }
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Share</Button>
         <Button size="small">Learn More</Button>
       </CardActions>
     </Card>
