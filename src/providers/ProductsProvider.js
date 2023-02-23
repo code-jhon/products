807d94dc0282a0df7925e7e183d929b4776e8b22
jerikls <jhon.avila@loadsure.net>
2023-02-23 00:03:08 -0500

    feat: adding flag state for modal

--- src/providers/ProductsProvider.js
+++ src/providers/ProductsProvider.js
@@ -1,13 +1,14 @@
-import { createContext } from 'react'
+import { useState, createContext } from 'react'
 import { useProducts } from '../hooks/products';
 
 export const ProductsContext = createContext();
 
 export const ProductsProvider = props => {
-  const [products, fetched] = useProducts();
+  const [products, fetched] = useProducts()
+  const [isModalEnabled, setIsModalEnabled] = useState(false)
 
   return (
-    <ProductsContext.Provider value={{ products, fetched }} >
+    <ProductsContext.Provider value={{ products, fetched, isModalEnabled, setIsModalEnabled }} >
       { props.children }
     </ProductsContext.Provider>
   )
